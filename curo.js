c71afcf40addbff5a2bdc0eebaee89c441d2cf04
Priya Singh <130273157+priya-169@users.noreply.github.com>
2023-04-12 01:39:31 +0530

    front-end Desogn of the SIH
    SMART INDIA HACKATHON

---
+++ curo.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $("#owl-demo").owlCarousel({
+        navigation : true, 
+        slideSpeed : 300,
+        paginationSpeed : 500,
+        items: 1,
+singleItem: true,
+        autoPlay : 4000
+});
+});
---
+++ script.js
@@ -0,0 +1,18 @@
+//  java Scriptfor navbar
+$(document).ready(function () {
+  $('.btn').click(function () {
+    $('.items').toggleClass("show");
+    $('ul li').toggleClass("hide");
+  });
+});
+
+
+
+
+s
+//For Searchbar
+
+
+  // java Script for Project section 
+
+
